d6a44b531b7123e0fa825a85cd4e986402517145
Paton Vinal <paton.vinal@gmail.com>
2017-05-25 16:43:01 -0400

    working parallel promises and top tracks

--- spotify.js
+++ spotify.js
@@ -11,6 +11,35 @@ const getFromApi = function(endpoint, query={}) {
   });
 };
 
+/*
+
+// Chained Promises
+myFunction().then((data) => {
+    return myOtherFunction(data)
+}).then((response) => {
+    return myThirdFunction(data)
+}).then((whatever) => {
+    console.log(whatever)
+}).then((response) => {
+    return myThirdFunction(data)
+}).then((whatever) => {
+    console.log(whatever)
+})
+
+myFunction().then(data => {
+    return myOtherFunction.then(() => {
+        return myThirdFunction().then(() => {
+            return myOtherFunction.then(() => {
+                
+                return myThirdFunction().then(() => {
+                    console.log(whatefver)
+                })
+            })
+        })
+    })
+})
+
+*/
 
 let artist = null;
 const getArtist = function(name) {
@@ -22,25 +51,27 @@ const getArtist = function(name) {
   return getFromApi('search', query).then(response => {
     artist = response.artists.items[0];
     const id = artist.id;
-    return getFromApi(`artists/${id}/related-artists`, query).then(response => {
-      artist.related = response.artists;
-      let allArtistID = [];
-      for (let i=0; i<artist.related.length; i++){
-        let artistID = artist.related[i].id;
-        allArtistID.push(getFromApi(`artists/${artistID}/top-tracks?country=US`, query));
-      }
-      const allPromises = Promise.all(allArtistID).then(responses => {
-        for (let i=0; i<allPromises.length; i++){
-          artist.related['tracks'] = responses[i].tracks;
-        }
-      });
-      console.log(allPromises);
-      return artist;
-    });
-  }).catch (err => {
-    console.error(err);
+    return getFromApi(`artists/${id}/related-artists`, query);
+  }).then(response => {
+    artist.related = response.artists;
+    let allArtistID = [];
+    for (let i=0; i<artist.related.length; i++){
+      let artistID = artist.related[i].id;
+      allArtistID.push(getFromApi(`artists/${artistID}/top-tracks?country=US`, query));
+    }
+    return Promise.all(allArtistID);
+  }).then(responses => {
+    for (let i=0; i<responses.length; i++){
+      artist.related[i].tracks = responses[i].tracks;
+    }
+    return artist;
   });
 };
+      
+/*.catch (err => {
+    console.error(err);*/
+//   });
+// };
 
   
   
