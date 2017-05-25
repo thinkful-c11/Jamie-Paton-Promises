fd2e2e64b06086a8d4143aa70bb72816552c868b
Paton Vinal <paton.vinal@gmail.com>
2017-05-25 12:46:36 -0400

    initial commit and first promise exercise

---
+++ spotify.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const getFromApi = function(endpoint, query={}) {
+  const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
+  Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+  return fetch(url).then(function(response) {
+    if (!response.ok) {
+      return Promise.reject(response.statusText);
+    }
+    return response.json();
+  });
+};
+
+
+let artist = null;
+const getArtist = function(name) {
+  const query = {
+    q: name,
+    limit: 1,
+    type: 'artist'
+  };
+  return getFromApi('search', query).then(response => {
+    artist = response.artists.items[0];
+    return artist;
+  });
+};
+
